850b1fba9f5c7d54f20123f57341ae39bedfacb7
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-06 14:41:24 +0200

    Snack 3 complete

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -25,20 +25,40 @@ Console.WriteLine("Hello, World!");
 
 //SNAKC 2
 
-Console.WriteLine("Immettere prima Parola");
-string primaParola = Console.ReadLine();
+//Console.WriteLine("Immettere prima Parola");
+//string primaParola = Console.ReadLine();
 
-Console.WriteLine("Immettere seconda Parola");
-string secondaParola = Console.ReadLine();
+//Console.WriteLine("Immettere seconda Parola");
+//string secondaParola = Console.ReadLine();
 
-if (primaParola.Length < secondaParola.Length)
+//if (primaParola.Length < secondaParola.Length)
+//{
+//    Console.WriteLine($"La parola è più corta è: {primaParola}");
+//    Console.WriteLine($"La parola più lunga è: {secondaParola} ");
+
+//}
+//else if (primaParola.Length > secondaParola.Length)
+//{
+//    Console.WriteLine($"La parola è più corta è: {secondaParola}");
+//    Console.WriteLine($"La parola più lunga è: {primaParola}");
+//}
+
+//SNACK 3 
+
+int somma = 0;
+
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine($"La parola è più corta è: {primaParola}");
-    Console.WriteLine($"La parola più lunga è: {secondaParola} ");
+    Console.WriteLine($"Inserisci il numero {i + 1}");
+    if (int.TryParse( Console.ReadLine(), out int numero ) )
+    {
+        somma += numero;
+    }
+    else
+    {
+        Console.WriteLine("Carattere sconosciuto. Inserisci un numero.");
+        i--;
+    }
+}
+Console.WriteLine($"La somma di tutti i numeri inseriti è: {somma}");
 
-}
-else if (primaParola.Length > secondaParola.Length)
-{
-    Console.WriteLine($"La parola è più corta è: {secondaParola}");
-    Console.WriteLine($"La parola più lunga è: {primaParola}");
-}
