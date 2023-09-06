2d2d684026e2fda1cd9853b5c6df28886dd2784f
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-06 14:24:03 +0200

    Secondo Snakc complete

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -3,24 +3,42 @@ Console.WriteLine("Hello, World!");
 
 
 //SNACK 1 
-Console.WriteLine("Inserisci il primo numero:");
-if (int.TryParse(Console.ReadLine(), out int numero1))
+//Console.WriteLine("Inserisci il primo numero:");
+//if (int.TryParse(Console.ReadLine(), out int numero1))
+//{
+//    Console.WriteLine("Inserisci secondo numero:");
+//    if (int.TryParse(Console.ReadLine(), out int numero2))
+//    {
+//        int numeroMaggiore = Math.Max(numero1, numero2);
+//        Console.WriteLine($"il numero maggiore tra {numero1} e {numero2} é: {numeroMaggiore} ");
+//    }
+//    else
+//    {
+//        Console.WriteLine("il numero secondo numero inserito non è valido ");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("il primo numero inserito non è valido ");
+//}
+
+
+//SNAKC 2
+
+Console.WriteLine("Immettere prima Parola");
+string primaParola = Console.ReadLine();
+
+Console.WriteLine("Immettere seconda Parola");
+string secondaParola = Console.ReadLine();
+
+if (primaParola.Length < secondaParola.Length)
 {
-    Console.WriteLine("Inserisci secondo numero:");
-    if (int.TryParse(Console.ReadLine(), out int numero2))
-    {
-        int numeroMaggiore = Math.Max(numero1, numero2);
-        Console.WriteLine($"il numero maggiore tra {numero1} e {numero2} é: {numeroMaggiore} ");
-    }
-    else
-    {
-        Console.WriteLine("il numero secondo numero inserito non è valido ");
-    }
+    Console.WriteLine($"La parola è più corta è: {primaParola}");
+    Console.WriteLine($"La parola più lunga è: {secondaParola} ");
+
 }
-else
+else if (primaParola.Length > secondaParola.Length)
 {
-    Console.WriteLine("il primo numero inserito non è valido ");
+    Console.WriteLine($"La parola è più corta è: {secondaParola}");
+    Console.WriteLine($"La parola più lunga è: {primaParola}");
 }
-
-
-
