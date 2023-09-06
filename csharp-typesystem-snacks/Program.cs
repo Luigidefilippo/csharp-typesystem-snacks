81f762a9ed539b5480f37a67b1031be2d62feac6
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-06 14:13:49 +0200

    Aggiungere i file di progetto.

---
+++ csharp-typesystem-snacks/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//SNACK 1 
+Console.WriteLine("Inserisci il primo numero:");
+if (int.TryParse(Console.ReadLine(), out int numero1))
+{
+    Console.WriteLine("Inserisci secondo numero:");
+    if (int.TryParse(Console.ReadLine(), out int numero2))
+    {
+        int numeroMaggiore = Math.Max(numero1, numero2);
+        Console.WriteLine($"il numero maggiore tra {numero1} e {numero2} é: {numeroMaggiore} ");
+    }
+    else
+    {
+        Console.WriteLine("il numero secondo numero inserito non è valido ");
+    }
+}
+else
+{
+    Console.WriteLine("il primo numero inserito non è valido ");
+}
+
+
+
